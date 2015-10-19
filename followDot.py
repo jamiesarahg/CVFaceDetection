ced6d60b1fcbd9dd448ddc4f986c6a6d867da20d
Jamie Gorson <jamie.gorson@students.olin.edu>
2015-10-19 16:41:28 -0400

    dot moves one iteration

--- followDot.py
+++ followDot.py
@@ -1,8 +1,61 @@
 import pygame
 import time
 
+white = (255,255,255)
+black = (0,0,0)
+width = 800
+height = 800
+sleepTime = .005
+
+x=width/2
+y=height/2
+
 pygame.init()
-screen = pygame.display.set_mode((640,640))
-pygame.draw.circle(screen, (255,255,255), (320,320), 10)
+screen = pygame.display.set_mode((width,height))
+pygame.draw.circle(screen, white, (x,y), 5)
 pygame.display.update()
-time.sleep(10)
+time.sleep(sleepTime)
+
+
+
+while x < width:
+	screen.fill(black)
+	pygame.draw.circle(screen, white, (x,y), 5)
+	pygame.display.update()
+	x+=1
+	time.sleep(sleepTime)
+
+while x >0:
+	screen.fill(black)
+	pygame.draw.circle(screen, white, (x,y), 5)
+	pygame.display.update()
+	x-=1
+	time.sleep(sleepTime)
+
+while x<width/2:
+	screen.fill(black)
+	pygame.draw.circle(screen, white, (x,y), 5)
+	pygame.display.update()
+	x+=1
+	time.sleep(sleepTime)
+
+while y < height:
+	screen.fill(black)
+	pygame.draw.circle(screen, white, (x,y), 5)
+	pygame.display.update()
+	y+=1
+	time.sleep(sleepTime)
+
+while y >0:
+	screen.fill(black)
+	pygame.draw.circle(screen, white, (x,y), 5)
+	pygame.display.update()
+	y-=1
+	time.sleep(sleepTime)
+
+while y<height/2:
+	screen.fill(black)
+	pygame.draw.circle(screen, white, (x,y), 5)
+	pygame.display.update()
+	y+=1
+	time.sleep(sleepTime)
