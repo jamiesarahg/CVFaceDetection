d831eed00931a883a36c88558d0e48e306672b56
Jamie Gorson <jamie.gorson@students.olin.edu>
2015-10-19 15:37:41 -0400

    change to pygame

--- followDot.py
+++ followDot.py
@@ -1,13 +1,8 @@
-from visual import *
+import pygame
 import time
- 
-time.sleep(1)
-scene2 = display(title='Follow the Ball',
-     x=0, y=0, width=800, height=800,
-     center=(300,300,0))
-ball = sphere(pos=(301,301,0), radius = 10, color=color.white)
-ball.velocity = vector(0,0,0)
-deltat = 0/005
-t=0
-ball.pos = ball.pos + ball.velocity*deltat
- 
+
+pygame.init()
+screen = pygame.display.set_mode((640,640))
+pygame.draw.circle(screen, (255,255,255), (320,320), 10)
+pygame.display.update()
+time.sleep(10)
