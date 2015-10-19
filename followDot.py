1cfbc1eafda914602a6d51bc2641a45987f7b855
Jamie Gorson <jamie.gorson@students.olin.edu>
2015-10-19 15:26:07 -0400

    started dot follow

--- followDot.py
+++ followDot.py
@@ -1,1 +1,13 @@
-'code'
+from visual import *
+import time
+ 
+time.sleep(1)
+scene2 = display(title='Follow the Ball',
+     x=0, y=0, width=800, height=800,
+     center=(300,300,0))
+ball = sphere(pos=(301,301,0), radius = 10, color=color.white)
+ball.velocity = vector(0,0,0)
+deltat = 0/005
+t=0
+ball.pos = ball.pos + ball.velocity*deltat
+ 
